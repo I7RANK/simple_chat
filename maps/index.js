52e0c533596eba820af478fdd6a9f48d3f5a6f29
I7RANK <franfgh2000@hotmail.com>
2022-01-15 23:26:45 -0500

    Uploading test connection to aws socket-io server

--- maps/index.js
+++ maps/index.js
@@ -1,3 +1,30 @@
+const url = 'http://ec2-3-138-155-24.us-east-2.compute.amazonaws.com:3001';
+
+const socket = io(url);
+
+socket.on("connect", () => {
+  console.log("I'm connected");
+  console.log(socket.id);
+
+  if (navigator.geolocation) {
+    // watch for user movement
+    navigator.geolocation.watchPosition(function(position) {
+      var lat = position.coords.latitude;
+      var lng = position.coords.longitude;
+
+      const obj = {
+        socketId: socket.id,
+        lat,
+        lng
+      };
+
+      socket.emit('test-location', obj);
+    });
+  } else {
+    alert("Geolocation is not supported by this browser.");
+  }
+});
+
 var map;
       var marker;
 
